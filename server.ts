beeeab6a3c945ec922081d94299f099d890784fc
Priyanshu <priyanshupaswan648@gmail.com>
2026-06-30 14:19:38 +0530

    Fix: Adjust static asset serving path in production to point to root dist

--- server.ts
+++ server.ts
@@ -1034,9 +1034,9 @@ ${userContextStr}`;
     });
     app.use(vite.middlewares);
   } else {
-    app.use(express.static(path.join(__dirname, "dist")));
+    app.use(express.static(path.join(__dirname, "..", "dist")));
     app.get("*", (_req, res) => {
-      res.sendFile(path.join(__dirname, "dist", "index.html"));
+      res.sendFile(path.join(__dirname, "..", "dist", "index.html"));
     });
   }
 
